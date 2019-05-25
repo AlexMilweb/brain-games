393b75f07653acf8ee9c7cfb70e64ec8c0d74a24
Alexander Miliutin <milya89@gmail.com>
2019-05-25 20:26:20 +0300

    Updates

---
+++ precache-manifest.76eb1ad66d4b649b5f86ace3021af19c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c91d655010b32320bdca9b8893499ff",
+    "url": "/brain-games/index.html"
+  },
+  {
+    "revision": "615ac856d0caec982524",
+    "url": "/brain-games/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "3839c88d99fd6c376fb8",
+    "url": "/brain-games/static/js/2.8f8c908b.chunk.js"
+  },
+  {
+    "revision": "615ac856d0caec982524",
+    "url": "/brain-games/static/js/main.71204c7e.chunk.js"
+  },
+  {
+    "revision": "6c2221df41b463187f3f",
+    "url": "/brain-games/static/js/runtime~main.0e80d99e.js"
+  }
+]);
